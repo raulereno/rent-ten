87e028269db5ebc3f49f072792029d1719271272
Zkcast <castioniezequiel@gmail.com>
2022-11-03 00:44:29 -0300

    DB and models created

---
+++ api/src/models/User.js
@@ -0,0 +1,65 @@
+const { DataTypes } = require('sequelize');
+
+
+
+module.exports = (sequelize) => {
+
+    sequelize.define('User', {
+
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true
+        },
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        mail: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        country: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        admin: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
+
+        favoriteshouses: {
+            type: DataTypes.ARRAY(DataTypes.STRING),
+            allowNull: false,
+        },
+
+
+    
+    },
+    
+    {
+        timestamps: false,
+    }
+    
+    )
+}
+// {
+//     id:
+//     name:
+//     lastname: 
+//     mail:
+//     country:
+//     favoritesHouses: []
+//     ownerOf: [] 
+//     }
