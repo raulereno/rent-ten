680dae95c3574f58b6691986f834c5b27040364e
Jhony A Saenz <jash0310@gmail.com>
2022-11-05 09:00:39 -0500

    creo barra navbar con material desing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { AuthModule } from '@auth0/auth0-angular'
+import { AuthModule } from '@auth0/auth0-angular';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { AboutComponent } from './components/about/about.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { environment as env } from 'src/environments/environment';
+import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -16,16 +21,22 @@ import { environment as env } from 'src/environments/environment';
     LoginComponent,
     AboutComponent,
     NavbarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
       { path: 'login', component: LoginComponent },
       { path: 'about', component: AboutComponent },
     ]),
     AuthModule.forRoot({
-      ... env.auth,
-    })
+      ...env.auth,
+    }),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
