c86177396bf0f22f1423e0b54665a6ec0fec5f39
Arii Jackson <ariijackson@hotmail.com>
2022-11-10 18:10:24 -0300

    Cambios

--- api/src/routes/houses.js
+++ api/src/routes/houses.js
@@ -132,10 +132,14 @@ router.delete("/deletehouse", async (req, res) => {
 
 router.post("/fulldb", async (req, res) => {
   try {
-    extraHouses.forEach(async (house) => {
-      let finder = await House.findOne({ where: house });
-      if (!finder) {
-        await House.create(house);
+    extraHouses(30).forEach(async (house) => {
+      try {
+        let finder = await House.findOne({ where: house });
+        if (!finder) {
+          await House.create(house);
+        }
+      } catch (error) {
+        console.log(error)
       }
     });
 
