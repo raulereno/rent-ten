f612225f5aa46ac7cf8253e668b156a6d2dfcb98
Arii Jackson <ariijackson@hotmail.com>
2022-11-19 20:17:32 -0300

    Modificando about

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+const { default: axios } = require("axios");
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
@@ -5,5 +6,6 @@ const { conn } = require("./src/db.js");
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
+    //axios.post("http://localhost:3001/houses/fulldb");
   });
 });
--- client/src/app/models/about.interface.ts
+++ client/src/app/models/about.interface.ts
@@ -14,7 +14,7 @@ export const CREATORS: About[] = [
     name: 'Ezequiel',
     lastName: 'Castioni',
     image: '',
-    info: 'FullStack Developer',
+    info: 'Full Stack Web Developer',
     gitHub: 'https://github.com/Zkcast',
     linkIn: 'https://www.linkedin.com/in/ezequiel-guido-castioni-448996243/'
   },
@@ -23,7 +23,7 @@ export const CREATORS: About[] = [
     name: 'Raul',
     lastName: 'Ereño',
     image: 'https://images-ext-1.discordapp.net/external/uILy34hP00QAwTPI_06s-nD7X_h7IKDyJxwqMXu3XcU/https/res.cloudinary.com/dbgpp8nla/image/upload/v1667957367/e66nv5aqom1e2bkmen0f.jpg?width=498&height=498',
-    info: 'FullStack Developer',
+    info: 'Full Stack Web Developer',
     gitHub: 'https://github.com/raulereno',
     linkIn: 'https://www.linkedin.com/in/raulereno/'
   },
@@ -31,8 +31,8 @@ export const CREATORS: About[] = [
     id: 3,
     name: 'Jhony',
     lastName: 'Saenz Hurtado',
-    image: '',
-    info: 'FullStack Developer',
+    image: 'https://res.cloudinary.com/dbgpp8nla/image/upload/v1668898845/uqdscb1mhxqiz6jzlrgv.jpg',
+    info: 'Full Stack Web Developer',
     gitHub: 'https://github.com/jasaenzh',
     linkIn: 'https://www.linkedin.com/in/jhony-saenz-hurtado/'
   },
@@ -40,8 +40,8 @@ export const CREATORS: About[] = [
     id: 4,
     name: 'Diana',
     lastName: 'Terrango',
-    image: '',
-    info: 'FullStack Developer',
+    image: 'https://res.cloudinary.com/dbgpp8nla/image/upload/v1668887349/dgkfsmjmsa7ryh7qaiks.jpg',
+    info: 'Full Stack Web Developer',
     gitHub: 'https://github.com/Diaterra',
     linkIn: 'https://www.linkedin.com/in/diana-terragno/'
   },
@@ -50,7 +50,7 @@ export const CREATORS: About[] = [
     name: 'Leandro',
     lastName: 'De Filippis',
     image: '',
-    info: 'FullStack Developer',
+    info: 'Full Stack Web Developer',
     gitHub: 'https://github.com/leandefilippis',
     linkIn: 'https://www.linkedin.com/in/lean-de-filippis-33ab60202/'
   },
@@ -58,7 +58,7 @@ export const CREATORS: About[] = [
     id: 6,
     name: 'Ariana',
     lastName: 'Amaya Natel',
-    image: 'https://res.cloudinary.com/dbgpp8nla/image/upload/v1668562036/vctyo5yh6vkikudbsbhz.jpg',
+    image: 'https://res.cloudinary.com/dbgpp8nla/image/upload/v1668898884/eipeywxilye3soog2nne.jpg',
     info: 'FullStack Developer',
     gitHub: 'https://github.com/AriiJackson',
     linkIn: 'https://www.linkedin.com/in/ariana-virginia-amaya-natel/'
