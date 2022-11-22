97c60549b9130012f07871527ad6a6bf383808ed
raulereno <raulereno@gmail.com>
2022-11-22 12:16:47 -0300

    agregando chat bot

--- api/index.js
+++ api/index.js
@@ -1,12 +1,32 @@
-const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const axios = require("axios")
+const axios = require("axios");
+const server = require("./src/app.js");
+const http = require("http").Server(server);
+
+const io = require("socket.io")(http, {
+  cors: {
+    origin: true,
+    credentials: true,
+    allowedHeaders: ["my-custom-header"],
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log("Nuevo usuario conectado");
+  console.log(socket);
+  socket.on("sendMessage", (messageInfo) => {
+    console.log(messageInfo);
+    socket.broadcast.emit("receiveMessage", messageInfo);
+  });
+});
 
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
     axios.post("http://localhost:3001/houses/fulldb");
-
   });
 });
- 
+
+http.listen(3000, () => {
+  console.log(`Server on port ${3000}`);
+});
