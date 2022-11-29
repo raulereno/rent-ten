ea211b2829521289599d317555bb8d7df2ad1f6c
Jhony A Saenz <jash0310@gmail.com>
2022-11-29 17:18:16 -0500

    actualizando a la rama develop

---
+++ api/extra-db/jhony.bd.js
@@ -0,0 +1,67 @@
+const dbJhony = [
+    {
+        country: 'Colombia',
+        city: 'Cartagena',
+        type: 'House',
+        picture: [
+            "https://a0.muscache.com/im/pictures/miso/Hosting-560010173485132633/original/e1e49284-0a6d-427f-aa7d-cba81ae53e23.jpeg?im_w=1200",
+            "https://a0.muscache.com/im/pictures/miso/Hosting-560010173485132633/original/bd77cfed-869c-4896-af27-74162a5ff801.jpeg?im_w=720",
+            "https://a0.muscache.com/im/pictures/miso/Hosting-560010173485132633/original/20dbe4b2-1318-40ef-a700-0054db7e786a.jpeg?im_w=720",
+            "https://a0.muscache.com/im/pictures/miso/Hosting-560010173485132633/original/3d1af9aa-995c-4700-b73e-1f3bbcbebd17.jpeg?im_w=720",
+            "https://a0.muscache.com/im/pictures/miso/Hosting-560010173485132633/original/91c1302d-c2f2-4e34-b208-f17c244b498f.jpeg?im_w=720",
+            "https://a0.muscache.com/im/pictures/miso/Hosting-560010173485132633/original/91c1302d-c2f2-4e34-b208-f17c244b498f.jpeg?im_w=1200"
+        ]
+    },
+    {
+        country: 'Colombia',
+        city: 'Cartagena',
+        type: 'House',
+        picture: [
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        country: 'Colombia',
+        city: 'Cartagena',
+        type: 'House',
+        picture: [
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        country: 'Colombia',
+        city: 'Cartagena',
+        type: 'House',
+        picture: [
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        country: 'Colombia',
+        city: 'Cartagena',
+        type: 'House',
+        picture: [
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+]
