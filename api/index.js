b2fb50df9196b86a6267566d56d0127ebc71f912
Leandefilippis <leandefilippis@gmail.com>
2022-11-30 23:46:43 -0300

    Merging with develop

--- api/index.js
+++ api/index.js
@@ -5,6 +5,6 @@ const server = require("./src/app.js");
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
-    axios.post("http://localhost:3001/houses/fulldb");
+    // axios.post("http://localhost:3001/houses/fulldb");
   });
 });
